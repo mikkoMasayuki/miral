4e05ffd6a2f0009ff7cd5f137fbd7ea18367e1d8
Mikko-Aasahi <mikko@createdbyblack.com>
2022-08-14 23:58:27 +0800

    Map Changes

--- src/maps/maplist.js
+++ src/maps/maplist.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import googleMapStyles from './GoogelMapStyle.js';
 import $ from 'jquery';
+import InfoWindowEx from "./infoMap.js";
 
 	
 export class MapContainer extends Component {
@@ -37,14 +38,14 @@ export class MapContainer extends Component {
 		this.inputElement.click();
 	  }
 
-	onMarkerClick = (props, marker, e) =>
-		
+	onMarkerClick = (props, marker, e) =>{
 	  this.setState({
 		selectedPlace: props,
 		activeMarker: marker,
 		showingInfoWindow: true,
 		selectMark:true
 	  });
+	}
   
 	onClose = props => {
 	  if (this.state.showingInfoWindow) {
@@ -59,6 +60,81 @@ export class MapContainer extends Component {
 		const test = window.document.querySelectorAll('div[title="Abu Dhabi Plaza"]')
 
 		test.forEach(el=>el.click());
+	}
+
+	onFave = (place, marker, e) => {
+		const elem = window.document.getElementById(`star_${place.projId}`);
+		
+		console.log(place)
+		if (place.fav == 0) {
+			const requestOptions = {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ 
+					id: place.projId,
+					is_favorite:1
+				})
+			};
+			elem.classList.add("active");
+
+	
+			fetch('http://54.183.107.251:4052/project', requestOptions)
+			.then(response => {
+				
+				fetch('http://54.183.107.251:4052/project')
+				.then((response) => response.json())
+				.then(projectlist => {
+					this.setState({
+						 projects: projectlist.data 
+					});
+	  
+					if (this.state.showingInfoWindow) {
+					  this.setState({
+						showingInfoWindow: false,
+						selectedPlace: place,
+						activeMarker: null
+					  });
+					}
+				});
+			});
+
+
+			
+		}else{
+			const requestOptions = {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ 
+					id: place.projId,
+					is_favorite:0
+				})
+			};
+
+			elem.classList.remove("active");
+
+	
+			fetch('http://54.183.107.251:4052/project', requestOptions)
+			.then(response => {
+				fetch('http://54.183.107.251:4052/project')
+				.then((response) => response.json())
+				.then(projectlist => {
+					this.setState({
+						 projects: projectlist.data 
+					});
+	  
+					if (this.state.showingInfoWindow) {
+					  this.setState({
+						showingInfoWindow: false,
+						selectedPlace: place,
+						activeMarker: null
+					  });
+					}
+				});
+			});
+
+			
+		}
+
 	}
 
 	onGeneral = props => {
@@ -80,7 +156,6 @@ export class MapContainer extends Component {
 		})
 	 }
   render() {
-	console.log(this.state.selectedPlace)
     return (
 	<React.Fragment>
 
@@ -132,6 +207,8 @@ export class MapContainer extends Component {
 							.reduce((prev, curr) => [prev, '', curr])}
 							image={project.image[0].image_url}
 							title={project.name}
+							fav={project.is_favorite}
+							projId={project.id}
 							ref={() => { 
 								const markVar = this.state.selectMark
 								function selectMap(selectMark = markVar){
@@ -165,13 +242,14 @@ export class MapContainer extends Component {
 
 						}
 						
-								<InfoWindow
+								<InfoWindowEx
 								marker={this.state.activeMarker}
-								visible={this.state.showingInfoWindow}>
+								visible={this.state.showingInfoWindow}
+								>
 									
 
-									<div className=" item"><a className="link" > </a><div className="ftrd_img"><img src={this.state.selectedPlace.image} /></div><div className="desc"><svg className="star_icon" width="25" height="25" xmlns="http://www.w3.org/2000/svg" id="" data-name="Layer 1" viewBox="0 0 122.88 117.1"><path stroke="#9d9d9c" stroke-width="3" fill="none" className="cls-1" d="M64.42,2,80.13,38.7,120,42.26a3.2,3.2,0,0,1,1.82,5.62h0L91.64,74.18l8.9,39A3.19,3.19,0,0,1,98.12,117a3.27,3.27,0,0,1-2.46-.46L61.41,96.1,27.07,116.64a3.18,3.18,0,0,1-4.38-1.09,3.14,3.14,0,0,1-.37-2.38h0l8.91-39L1.09,47.88a3.24,3.24,0,0,1-.32-4.52,3.32,3.32,0,0,1,2.29-1l39.72-3.56L58.49,2a3.24,3.24,0,0,1,5.93,0Z"></path></svg><h4>{this.state.selectedPlace.name}</h4><p>{this.state.selectedPlace.location}</p><p className="tag tag_1">{this.state.selectedPlace.type}</p><p className="tag tag_2">{this.state.selectedPlace.role}</p></div></div>
-								</InfoWindow>
+									<div className=" item" ><a className={`link`}  > </a><div className="ftrd_img"><img src={this.state.selectedPlace.image} /></div><div className="desc"><svg className={`star_icon ${ this.state.selectedPlace.fav && 'active'}`} id={`star_${this.state.selectedPlace.projId}`} onClick={this.onFave.bind(this, this.state.selectedPlace)} width="25" height="25" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 122.88 117.1"><path stroke="#9d9d9c" stroke-width="3" fill="none" className="cls-1" d="M64.42,2,80.13,38.7,120,42.26a3.2,3.2,0,0,1,1.82,5.62h0L91.64,74.18l8.9,39A3.19,3.19,0,0,1,98.12,117a3.27,3.27,0,0,1-2.46-.46L61.41,96.1,27.07,116.64a3.18,3.18,0,0,1-4.38-1.09,3.14,3.14,0,0,1-.37-2.38h0l8.91-39L1.09,47.88a3.24,3.24,0,0,1-.32-4.52,3.32,3.32,0,0,1,2.29-1l39.72-3.56L58.49,2a3.24,3.24,0,0,1,5.93,0Z"></path></svg><h4>{this.state.selectedPlace.name}</h4><p>{this.state.selectedPlace.location}</p><p className="tag tag_1">{this.state.selectedPlace.type}</p><p className="tag tag_2">{this.state.selectedPlace.role}</p></div></div>
+								</InfoWindowEx>
 						
 						
 					</Map>
