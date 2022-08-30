8a104017aa00ada199e9ac659b57df20e767c5c6
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-08-31 02:49:53 +0800

    destination url change

--- src/admin/ProjectRow.js
+++ src/admin/ProjectRow.js
@@ -6,7 +6,7 @@ const ProjectRow = props => {
 
         <div className="col-md-12">
 			<div class="inner_wrap">
-				<div class="box"><p> <Link to={`/admin/update/${props.id}`} className='link' >{props.name} <span>{props.status == 2 ? 'Draft' : 'Publish'}</span></Link></p></div>
+				<div class="box"><p className={props.status == 2 ? 'jsx_orange' : 'jsx_black'}> <Link to={`/admin/update/${props.id}`} className='link' >{props.name} <span>{props.status == 2 ? 'Draft' : 'Publish'}</span></Link></p></div>
 			
            </div>
         </div>
