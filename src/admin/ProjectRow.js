9185df15faf849e2ebe090cf78af0c81c72d94c6
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-05 13:18:37 +0800

    edited spelling

--- src/admin/ProjectRow.js
+++ src/admin/ProjectRow.js
@@ -6,7 +6,7 @@ const ProjectRow = props => {
 
         <div className="col-md-12">
 			<div class="inner_wrap">
-				<div class="box"><p> <Link to={`/admin/update/${props.id}`} className='link' >{props.name} <span className={props.status == 2 ? 'jsx_orange' : 'jsx_black'}>{props.status == 2 ? 'Draft' : 'Publish'}</span></Link></p></div>
+				<div class="box"><p> <Link to={`/admin/update/${props.id}`} className='link' >{props.name} <span className={props.status == 2 ? 'jsx_orange' : 'jsx_black'}>{props.status == 2 ? 'Draft' : 'Published'}</span></Link></p></div>
 			
            </div>
         </div>
