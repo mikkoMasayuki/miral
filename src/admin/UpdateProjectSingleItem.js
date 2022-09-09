577107cc5a216e10c5c1d97f52430bc6251188c2
Remely <remely@createdbyblack.com>
2022-09-10 01:18:08 +0800

    admin image msg and logo update

--- src/admin/UpdateProjectSingleItem.js
+++ src/admin/UpdateProjectSingleItem.js
@@ -435,7 +435,7 @@ const UpdateProjectSingleItem = props => {
 					</fieldset>
 
 					<fieldset class="ima_ges">
-						<label>Images (JPG max file size 2mb):</label>
+						<label>Images (1600 x 1020 pixels JPG/WebP, optimum file size max 500Kb)</label>
 						<div class="item_wrap">
 							
 
