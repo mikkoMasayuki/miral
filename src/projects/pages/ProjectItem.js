836f9fd12e03acbabfecfdcb5aae42e624f764fb
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-12 19:45:34 +0800

    comma

--- src/projects/pages/ProjectItem.js
+++ src/projects/pages/ProjectItem.js
@@ -37,17 +37,15 @@ const ProjectItem = props => {
 
 
 	function putComma(val) {
-		let ret = ''
+		let ret = val
 		if (val === 'Yas Island Abu Dhabi') {
 			ret = 'Yas Island, Abu Dhabi'
-		} else {
-			ret = val
 		}
 		if (val === 'Saadiyat Island Abu Dhabi') {
 			ret = 'Saadiyat Island, Abu Dhabi'
-		} else {
-			ret = val
 		}
+
+		
 		return ret
 
 }
