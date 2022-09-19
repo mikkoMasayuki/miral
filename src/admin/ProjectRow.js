ce5f74b4001032184c7b969d4df913ada71207b5
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-19 16:13:05 +0800

    added sessions

--- src/admin/ProjectRow.js
+++ src/admin/ProjectRow.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 const ProjectRow = props => {
+
+
     return (
 
         <div className="col-md-12">
 			<div class="inner_wrap">
-				<div class="box"><p> <Link to={`/admin/update/${props.id}`} className='link' >{props.name ==='' ? '  ' : props.name } <span className={props.status == 2 ? 'jsx_orange' : 'jsx_black'}>{props.status == 2 ? 'Draft' : 'Published'}</span></Link></p></div>
+				<div class="box"><p><Link to={`/admin/update/${props.id}`} className='link' >{props.name ==='' ? '  ' : props.name } <span className={props.status == 2 ? 'jsx_orange' : 'jsx_black'}>{props.status == 2 ? 'Draft' : 'Published'}</span></Link></p></div>
 			
            </div>
         </div>
