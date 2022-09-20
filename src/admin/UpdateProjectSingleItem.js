b9f46e387af5ad4a66545dbdc0713ae3966a80a0
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-21 04:45:39 +0800

    remove htmlpraser

--- src/admin/UpdateProjectSingleItem.js
+++ src/admin/UpdateProjectSingleItem.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom'
 import Swal from 'sweetalert2'
-import parse from 'html-react-parser';
+
 
 
 
