3e75139f0f7624d25bd2f2e957066bed12cff6f1
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-25 00:41:48 +0800

    arab update

--- src/projects/pages/single/singleItem.js
+++ src/projects/pages/single/singleItem.js
@@ -8,6 +8,88 @@ const SingleItem = props => {
 	const [locationKeys, setLocationKeys] = useState([]);
 	const history = useHistory();
 
+
+    const queryParams = new URLSearchParams(window.location.search)
+    const lang = queryParams.get("l")    
+
+    let urlparams = "?l="+lang
+
+    let lang_about = 'About'
+	let lang_filter_projects = 'FILTER PROJECTS'
+	let lang_reset = 'RESET'
+	let lang_type = 'Type'
+	let lang_all = 'All'
+	let lang_most_popular = 'Most Popular'
+	let lang_stay = 'Stay'
+	let lang_play = 'Play'
+	let lang_dine = 'Dine'
+	let lang_business_venues = 'Business Venues'
+	let lang_culture = 'Culture'
+	let lang_destination = 'Destination'
+
+	let lang_location = 'Location'
+	let lang_yas_island = 'Yas Island'
+	let lang_abu_dhabi = 'Abu Dhabi'
+	let lang_saadiyat_island = 'Saadiyat Island'
+	let lang_al_ain = 'Al Ain'
+	let lang_jordan = 'Jordan'
+	let lang_kazakhstan = 'Kazakhstan'
+
+	let lang_business = 'Business'
+	let lang_miral = 'Miral'
+	let lang_miral_destinations = 'Miral Destinations'
+	let lang_miral_experiences = 'Miral Experiences'
+	let lang_yas_asset_management = 'Yas Asset Management'
+
+	let lang_completed = 'Completed'
+	let lang_size = 'Size'
+	let lang_website = 'Website'
+	let lang_readmore = 'Read more'
+
+	let lang_description = props.description
+	let lang_headline = props.headline_en
+
+
+    if(lang === null || lang!=='ar') {
+        urlparams = ""
+    } else {
+        lang_about = 'نبذة عن الحافظة'
+		lang_filter_projects = 'تصفية نتائج البحث'
+		lang_reset = 'إعادة ضبط'
+		lang_type = 'نوع'
+		lang_all = 'الكل'
+		lang_most_popular = 'الأكثر شيوعاً'
+		lang_stay = 'الفنادق'
+		lang_play = 'التجارب'
+		lang_dine = 'المطاعم'
+		lang_business_venues = 'مرافق فعاليات الأعمال'
+		lang_culture = 'ثقافة'
+		lang_destination = 'وجهات'
+
+		lang_location = 'الموقع'
+		lang_yas_island = 'جزيرة ياس'
+		lang_abu_dhabi = 'أبوظبي'
+		lang_saadiyat_island = 'جزيرة السعديات'
+		lang_al_ain = 'العين'
+		lang_jordan = 'الأردن'
+		lang_kazakhstan = 'كازخستان'		
+
+		lang_business = 'المنشأة'
+		lang_miral = 'ميرال'
+		lang_miral_destinations = 'ميرال ديستينيشنز'
+		lang_miral_experiences = 'ميرال إكسبيرينسز'
+		lang_yas_asset_management = 'ياس لإدارة الأصول'		
+
+		lang_completed = 'سنة الإنجاز:'
+		lang_size = 'المساحة (متر مربع):'
+		lang_website = 'الموقع الالكتروني:'		
+		lang_readmore = 'اقرأ المزيد'
+		lang_description = props.description_ar
+		lang_headline = props.headline_ar
+    }	
+
+
+
 	useEffect(() => {
 		return history.listen((location) => {
 		  if (history.action === "PUSH") {
@@ -99,7 +181,8 @@ props.images.map(image => {
 	}
 })
 
-console.log(real_images)
+
+//console.log(props.items)
 
     return <React.Fragment>
         <div class="row head_content">
@@ -122,15 +205,15 @@ console.log(real_images)
 						
 					</div>
 					<ul>
-						<li><strong>Completed:</strong> {props.year}</li>
-						<li><strong>Size (sq.m):</strong> {props.size}</li>
-						<li><strong>Website: </strong><a target="_blank" href={addHttp(props.website)}>{removeHttp(props.website)}</a></li>
+						<li><strong>{lang_completed}:</strong> {props.year}</li>
+						<li><strong>{lang_size} (sq.m):</strong> {props.size}</li>
+						<li><strong>{lang_website}: </strong><a target="_blank" href={addHttp(props.website)}>{removeHttp(props.website)}</a></li>
 					</ul>					
 				</div>
 
 				<div class=" content_right">
-				<h3>{props.headline_en}</h3>		
-				<p>{props.description}</p>
+				<h3>{lang_headline}</h3>		
+				<p>{lang_description}</p>
 
 			</div>
 
