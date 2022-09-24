1a99d8899dfaa91bf3ebaf4dc9f03a2e1d7896e7
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-25 00:42:08 +0800

    arab update

--- src/shared/components/Navigation/SinglePageHeader.js
+++ src/shared/components/Navigation/SinglePageHeader.js
@@ -3,29 +3,42 @@ import React from "react";
 import ReactDOM from "react-dom"
 
 const HomePageHeader = props => {
+    const queryParams = new URLSearchParams(window.location.search)
+    const lang = queryParams.get("l")    
 
+    let urlparams = "?l="+lang
+
+    if(lang === null) {
+        urlparams = ""
+    }
+    
+    const home_url = "/"+urlparams
+    const about_url = "/destination-happiness/index.html"+urlparams
+    const miral_url = "https://miral.ae/"+urlparams
+    const privacy_url = "/privacy-policy/index.html"+urlparams	
+    const arab_url ="#"+urlparams
 
     const content  = 
     <div>
 <nav class="navbar navbar-expand-lg  m_header nav_single">
 	<div class="container">
-		<a class="navbar-brand" href="/"><img class="logo" src="/assets/img/mrl-logo.svg" alt="" /></a>
+		<a class="navbar-brand" href={home_url}><img class="logo" src="/assets/img/mrl-logo.svg" alt="" /></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"><i class="lnr lnr-menu"></i></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNav">
 		   <ul class="navbar-nav ml-auto">
 		      <li class="nav-item">
-		        <a class="nav-link " href="/destination-happiness/index.html">ABOUT</a>
+		        <a class="nav-link " href={about_url}>ABOUT</a>
 		      </li>
 
 			  	<li class="nav-item">
-                    <a class="nav-link " target="_blank" href="https://miral.ae/">MIRAL.AE</a>
+                    <a class="nav-link " target="_blank" href={miral_url}>MIRAL.AE</a>
             	</li>
 		     
 
 		      <li class="nav-item lang ">
-		        	<a class="nav-link " href="#">العربية</a>
+		        	<a class="nav-link " href={arab_url}>العربية</a>
 		      </li>
 		     
 		    </ul>
