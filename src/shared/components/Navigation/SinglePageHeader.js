5e6081199f137196bb217ece7da9ddb5819c71f2
Jun Javier <56975375+jjavieriii@users.noreply.github.com>
2022-09-26 11:16:04 +0800

    update

--- src/shared/components/Navigation/SinglePageHeader.js
+++ src/shared/components/Navigation/SinglePageHeader.js
@@ -8,18 +8,23 @@ const HomePageHeader = props => {
 
     let urlparams = "?l="+lang
 	let lang_about = 'ABOUT'
+    const home_url = "/"+urlparams
+    const about_url = "/destination-happiness/index.html"+urlparams
+    const miral_url = "https://miral.ae/"+urlparams
+    const privacy_url = "/privacy-policy/index.html"+urlparams	
+    let lang_switcher = 'العربية'
+    let arab_url ="?l=ar"
 
     if(lang === null || lang!=='ar') {
         urlparams = ""
     } else {
 		lang_about = 'نبذة عن الحافظة'
+		lang_switcher = 'ENGLISH'	
+		arab_url ="?l=en"		
 	}
     
-    const home_url = "/"+urlparams
-    const about_url = "/destination-happiness/index.html"+urlparams
-    const miral_url = "https://miral.ae/"+urlparams
-    const privacy_url = "/privacy-policy/index.html"+urlparams	
-    const arab_url ="#"+urlparams
+
+	
 
     const content  = 
     <div>
@@ -41,7 +46,7 @@ const HomePageHeader = props => {
 		     
 
 		      <li class="nav-item lang ">
-		        	<a class="nav-link " href={arab_url}>العربية</a>
+		        	<a class="nav-link " href={arab_url}>{lang_switcher}</a>
 		      </li>
 		     
 		    </ul>
